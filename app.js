f4e7d52fb97270c86653c0c63ddec2ffdd2e68a6
Jasonbuller94 <jason_buller94@yahoo.com>
2023-05-07 20:27:16 -0500

    First Commit, finished project

--- app.js
+++ app.js
@@ -1,10 +1,86 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
+app.use(morgan("dev"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static("public"));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+});
 
-app.listen(PORT, () => {
+app.get("/posts/:id", (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  console.log(post);
+
+  if (!post.id) {
+    next("Post Not Found");
+    return;
+  }
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.send(err);
+});
+
+const { PORT = 1337 } = process.env;
+
+app.listen(PORT, (req, res) => {
   console.log(`App listening in port ${PORT}`);
 });
